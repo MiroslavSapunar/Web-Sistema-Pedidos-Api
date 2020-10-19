28fa7c8e00698a372e12101ff173aaa96621b0d1
Miroslav <psapunar@fi.uba.ar>
2020-10-19 20:12:40 -0300

    server fix

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
 
 //const uri = process.env.ATLASDB_URI;
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: false }
