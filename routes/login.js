8ca42ef93e11a4ccda27cd720182d1f425a2cb87
Miroslav <psapunar@fi.uba.ar>
2020-10-24 14:06:14 -0300

    login fix

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,11 @@ let User = require('../models/user.model');
 
 
 router.route('/:id').get((req, res) => {
-    //console.log(req.params.id);
     const user = {
         username: req.params.id
     }
     User.find(user)
         .then(user => {
-            //console.log(user[0].password);
             if(user.length > 0){
                 res.json(user[0].password)
             }else{
